e17bcd5f1d0b711dcfb14feb9383b97ddb856d62
danielkaczmarczyk <danielkaczmarczyk2@gmail.com>
2020-06-23 17:39:16 +0100

    amended folder naming

--- 39-props/src/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Header extends React.Component {
-  render() {
-    return <h1 className="header">{`${this.props.n} Paintings!`}</h1>;
-  }
-}
-
-export default Header;
